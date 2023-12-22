7711e51e585ce8478ef65d9c51dabe83a8a78ee2
Sude Akarçay <akarcaysude2@gmail.com>
2023-12-22 21:01:36 +0300

    ApiClient and ApiService classes added

---
+++ app/src/main/java/com/ticktick/util/ApiService.kt
@@ -0,0 +1,10 @@
+package com.ticktick.util
+
+import com.ticktick.model.User
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("posts")
+    fun getUser(): Call<User>
+}
