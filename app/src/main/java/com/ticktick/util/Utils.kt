f21a3052e6ee40f8d49274a0c73d6184b894f805
Sude Akarçay <akarcaysude2@gmail.com>
2023-12-23 13:57:26 +0300

    Utils under util package added

---
+++ app/src/main/java/com/ticktick/util/Utils.kt
@@ -0,0 +1,20 @@
+package com.ticktick.util
+
+import com.ticktick.model.User
+
+object Utils {
+    object Utils {
+
+        const val TAGFORLAGCAT=""
+
+        const val TABLENAME_USER=""
+        const val TABLENAME_ACTIVITY=""
+        const val DATABASENAME=""
+
+        var baseUrl: String = "https://api.hekolcu.com"
+        var baseUrlForImage: String = ""
+
+        lateinit var user: User
+
+    }
+}
