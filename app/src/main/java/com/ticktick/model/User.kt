c3beb3feaa408c1996024f497ad44b7dd318d3d2
Sude Akarçay <akarcaysude2@gmail.com>
2023-12-26 20:30:37 +0300

    user_id is change to String from Int

--- app/src/main/java/com/ticktick/model/User.kt
+++ app/src/main/java/com/ticktick/model/User.kt
@@ -2,14 +2,14 @@ package com.ticktick.model
 
 import com.google.gson.annotations.SerializedName
 
-class User (
+class User(
     @SerializedName("user_id")
-    val user_id: Int,
+    val user_id: String = "",
     @SerializedName("username")
     val user_name: String = "",
     @SerializedName("profile_photo")
-    val profile_photo: String = "")
-{
+    val profile_photo: String = ""
+) {
     override fun toString(): String {
         return "User\nName: $user_name"
     }
