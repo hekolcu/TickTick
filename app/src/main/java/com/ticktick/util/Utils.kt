5a2f89073c061acacc7324ddeecc195d0b8c43c3
Sude Akarçay <akarcaysude2@gmail.com>
2023-12-26 20:25:47 +0300

    Utils.kt updated

--- app/src/main/java/com/ticktick/util/Utils.kt
+++ app/src/main/java/com/ticktick/util/Utils.kt
@@ -5,14 +5,13 @@ import com.ticktick.model.User
 object Utils {
     object Utils {
 
-        const val TAGFORLAGCAT=""
+        const val TAGFORLAGCAT = ""
 
-        const val TABLENAME_USER=""
-        const val TABLENAME_ACTIVITY=""
-        const val DATABASENAME=""
+        const val TABLENAME_USER = ""
+        const val TABLENAME_ACTIVITY = ""
+        const val DATABASENAME = ""
 
-        var baseUrl: String = "https://api.hekolcu.com"
-        var baseUrlForImage: String = ""
+        var baseUrl: String = "https://api.hekolcu.com/"
 
         lateinit var user: User
 
