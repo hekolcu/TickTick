f2dbabdd171787d1d2bdcfff3ef670a761b221e2
hekolcu <hekolcu@gmail.com>
2023-12-27 19:57:00 +0300

    task view bug fix

--- app/src/main/java/com/ticktick/TasksActivity.kt
+++ app/src/main/java/com/ticktick/TasksActivity.kt
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.ticktick.adapters.TaskGroupAdapter
 import com.ticktick.databinding.ActivityTasksBinding
 import com.ticktick.model.Task
-import java.util.Date
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -19,16 +18,8 @@ class TasksActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val tasks = ArrayList<Task>()
-        tasks.add(Task("HI", "Hello", Date(2023 - 1900, 12 - 1, 26, 1, 0, 0)))
-        tasks.add(Task("HI 2", "Hello", Date(2023 - 1900, 12 - 1, 27, 1, 0, 0)))
 
-        tasks.add(Task("MY FIRST TASK", "Hello", Date(2023 - 1900, 12 - 1, 15, 0, 0, 0)))
-        tasks.add(Task("MY Second TASK", "Hello", Date(2023 - 1900, 12 - 1, 15, 0, 0, 0)))
-        tasks.add(Task("MY Third TASK", "Hello", Date(2023 - 1900, 12 - 1, 15, 0, 0, 0)))
-        tasks.add(Task("MY Fourth TASK", "Hello", Date(2023 - 1900, 12 - 1, 15, 0, 0, 0)))
 
-        tasks.add(Task("askjdlas", "Hello", Date(2023 - 1900, 12 - 1, 15, 1, 0, 0)))
-        tasks.add(Task("asjdkas", "Hello", Date(2023 - 1900, 12 - 1, 15, 1, 0, 0)))
 
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
         val today = Calendar.getInstance()
@@ -40,6 +31,7 @@ class TasksActivity : AppCompatActivity() {
             when {
                 dateFormat.format(taskDate.time) == dateFormat.format(today.time) -> "Today"
                 dateFormat.format(taskDate.time) == dateFormat.format(tomorrow.time) -> "Tomorrow"
+                taskDate.before(today) -> "Past" // Group as "Past" if the date is before today
                 else -> dateFormat.format(taskDate.time)
             }
         }
