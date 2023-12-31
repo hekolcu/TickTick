600addfada4f7788dbfef1fdc4542fbee8c6e524
Sude Akarçay <akarcaysude2@gmail.com>
2023-12-31 22:48:17 +0300

    notification feature added

--- app/src/main/java/com/ticktick/MainActivity.kt
+++ app/src/main/java/com/ticktick/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.ticktick
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.ticktick.databinding.ActivityMainBinding
@@ -16,11 +21,15 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var defaultInbox: Group? = null
+    private val CHANNEL_ID = "TICKTICKCHANNELID"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        createNotificationChannel()
 
         val tvm = ViewModelProvider(this)[TaskViewModel::class.java]
 
@@ -40,10 +49,41 @@ class MainActivity : AppCompatActivity() {
             defaultInbox?.let {
                 tasksIntent.putExtra("defaultInbox", it)
                 startActivity(tasksIntent)
+                sendNotification("Welcome", "Welcome to TickTick!")
             } ?: run {
                 Log.d("MAIN", "defaultInbox is null")
             }
         }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "TickTick Channel"
+            val descriptionText = "TickTick Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(title: String, message: String) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationId = 1
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
     private fun loadData(tvm: TaskViewModel) {
